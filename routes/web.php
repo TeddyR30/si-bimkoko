f05949b2942548e53e103ace937073515e5f74b0
TeddyR30 <103474957+TeddyR30@users.noreply.github.com>
2023-10-18 11:48:03 +0700

    beranda

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('bimbingans', [\App\Http\Controllers\BimbinganController::class, 'index']);
